59eef90413b930824e9aed6d064b14858efceef1
Velugp <velu.gpv@gmail.com>
2020-05-14 16:47:45 +0530

    Update SubDetailScreen.controller.js

--- controller/SubDetailScreen.controller.js
+++ controller/SubDetailScreen.controller.js
@@ -2,8 +2,9 @@ var that;
 sap.ui.define([
 	"fiori/controller/BaseController",
 	"fiori/model/formatter",
-	"sap/ui/model/json/JSONModel"
-], function (BaseController, formatter, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageBox"
+], function (BaseController, formatter, JSONModel,MessageBox) {
 	"use strict";
 
 	return BaseController.extend("fiori.controller.SubDetailScreen", {
@@ -11,6 +12,11 @@ sap.ui.define([
 		formatter: formatter,
 
 		onInit: function () {
+			// Start of add on 14.05.2020	
+			if (!this.oDialogSub) {
+				this.oDialogSub = sap.ui.xmlfragment("fiori.Fragment.DialogSub", this);
+			}
+			// End of add on 14.05.2020				
 			this.getRouter().getRoute("SubDetailScreen").attachPatternMatched(this._onObjectMatched, this);
 			var oTab = this.byId("subDetailsTab");
 			this.oTab = oTab;
@@ -34,18 +40,17 @@ sap.ui.define([
 						});
 						sap.ui.getCore().setModel(jModel, "subDetailsModelTotal");
 						delete Data["statewise"][i];
-						break;
 					}
 				}
 				that.byId("numconfirmed").setValue(that.formatter.groupNumber(data_total.confirmed));
 				that.byId("numRecovered").setValue(that.formatter.groupNumber(data_total.recovered));
 				that.byId("numDeaths").setValue(that.formatter.groupNumber(data_total.deaths));
 				that.byId("numActive").setValue(that.formatter.groupNumber(data_total.active));
-// Start of add on 13.05.2020
+				// Start of add on 13.05.2020
 				that.byId("numTodayCases").setValue(that.formatter.groupNumber(data_total.deltaconfirmed));
 				that.byId("numTodayRec").setValue(that.formatter.groupNumber(data_total.deltarecovered));
 				that.byId("numTodayDea").setValue(that.formatter.groupNumber(data_total.deltadeaths));
-// End of add on 13.05.2020				
+				// End of add on 13.05.2020				
 				data_state = Data["statewise"];
 				// Sorter function				
 				data_state.sort(function (a, b) {
@@ -58,6 +63,32 @@ sap.ui.define([
 				that.oTab.setModel(oModel);
 				that.pieChart();
 			});
+		},
+
+		todayCountSub: function (oEvent) {
+			this.path = oEvent.getSource().getBindingContext().getPath();
+			this.ind = this.path.split('/')[2];
+			this.ind = parseInt(this.ind);
+			this.deltaSub = this.oModel.oData.subDetailsModel[this.ind];
+			sap.ui.getCore().byId("fragCasesSub").setValue(this.deltaSub.deltaconfirmed);
+			sap.ui.getCore().byId("fragDeaSub").setValue(this.deltaSub.deltadeaths);
+			sap.ui.getCore().byId("fragRecSub").setValue(this.deltaSub.deltarecovered);
+			sap.ui.getCore().byId("dpTime").setValue(this.deltaSub.lastupdatedtime);
+			if (this.deltaSub.deltaconfirmed == "0" && this.deltaSub.deltadeaths == "0" && this.deltaSub.deltarecovered == "0") {
+				MessageBox.show("Last Updated Time: "+ this.deltaSub.lastupdatedtime + "",{
+					icon:"ERROR",
+					title:"NO UPDATE"
+				});
+				return;
+			} else {
+				this.title= this.deltaSub.state.concat("'s Today Count");
+				sap.ui.getCore().byId("dialogFrag").setTitle(this.title);
+				this.oDialogSub.open();
+			}
+		},
+
+		fragClose: function () {
+			this.oDialogSub.close();
 		},
 
 		onAfterRendering: function () {
@@ -93,18 +124,23 @@ sap.ui.define([
 		},
 
 		tabPress: function (oEvent) {
-			debugger;
 			var stateTotal = {};
 			stateTotal.state = oEvent.getSource().getCells()[0].getText();
 			stateTotal.cCases = oEvent.getSource().getCells()[1].getNumber();
 			stateTotal.aCases = oEvent.getSource().getCells()[2].getNumber();
 			stateTotal.death = oEvent.getSource().getCells()[3].getNumber();
 			stateTotal.recovered = oEvent.getSource().getCells()[4].getNumber();
-// Start of add on 13.05.2020
-		    stateTotal.deltaconfirmed = oEvent.getSource().getCells()[5].getNumber();
-		    stateTotal.deltadeaths = oEvent.getSource().getCells()[6].getNumber();
-		    stateTotal.deltarecovered = oEvent.getSource().getCells()[7].getNumber();
-// End of add on 13.05.2020
+			// Start of add on 13.05.2020
+
+			this.path = oEvent.getSource().getBindingContext().getPath();
+			this.ind = this.path.split('/')[2];
+			this.ind = parseInt(this.ind);
+			// this.delta = that.data_state[this.ind];
+			this.delta = this.oModel.oData.subDetailsModel[this.ind];
+			stateTotal.deltaconfirmed = this.delta.deltaconfirmed;
+			stateTotal.deltadeaths = this.delta.deltadeaths;
+			stateTotal.deltarecovered = this.delta.deltarecovered;
+			// End of add on 13.05.2020
 			var oJsonModel = new sap.ui.model.json.JSONModel();
 			oJsonModel.setData(stateTotal);
 			sap.ui.getCore().setModel(oJsonModel, "stateData");
@@ -155,13 +191,13 @@ sap.ui.define([
 				toolTip: {
 					visible: true
 				}
-				
+
 				// dataLabel: {
 				// 	visible: true,
 				// 	type: "value",
 				// 	hideWhenOverlap: false,
 				// }
-				
+
 			});
 
 			var feedSize = new sap.viz.ui5.controls.common.feeds.FeedItem({
