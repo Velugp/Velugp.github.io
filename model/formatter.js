382c5042db0c84f6bfc3ce94003bd7517138c424
Velugp <velu.gpv@gmail.com>
2021-04-17 14:43:50 +0530

    Update formatter.js

--- model/formatter.js
+++ model/formatter.js
@@ -7,6 +7,18 @@ sap.ui.define([], function () {
 				groupingEnabled: true
 			});
 			return oNumFormat.format(sNumber);
+		},
+
+		convertShort: function (sValue) {
+			var loc = "en-US";
+			var oLocale = new sap.ui.core.Locale(loc);
+			var oFormatOptions = {
+				style: "short",
+				decimals: 1,
+				shortDecimals: 3
+			};
+			var oFloatFormat = sap.ui.core.format.NumberFormat.getFloatInstance(oFormatOptions,oLocale);
+			return oFloatFormat.format(sValue);
 		}
 
 	};
